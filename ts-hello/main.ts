9c79f1e517b22223f0324b0ae06368615e251f0b
rypilu <rpiluski@gmail.com>
2021-03-31 09:27:37 -0400

    sections 1-3

---
+++ ts-hello/main.ts
@@ -0,0 +1,152 @@
+// function log(message){
+//   console.log(message);
+// }
+//
+// var message = "Hello World";
+//
+// log(message);
+
+// ***************************************************************************
+// declaring variables
+// let is limited to the block in which it is declared
+// while variables declared with var has the global scope
+// USE LET
+
+// function doSomething() {
+//   for (var i = 0; i < 5; i++){
+//     console.log(i);
+//   }
+//
+//   console.log('Finally: ' + i);
+//
+// }
+//
+// doSomething();
+
+// ***************************************************************************
+// types
+
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1,true,'a',false]
+//
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+//
+// enum Color {Red = 0, Green = 1, Blue = 2};
+// let backgroundColor = Color.Red;
+
+
+//
+// let count = 5;
+// count = 'a';
+
+// let a: number;
+// a = 1;
+// a = true;
+
+
+// ***************************************************************************
+// type assertions
+// let message;
+// let message = 'abc';
+// let endswithc = message.endsWith('c');
+
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+
+// ***************************************************************************
+// arrow functions
+
+// let log = function(message) {
+//   console.log(message);
+// }
+
+// shorter way to do the above:
+
+// let doLog = (message) => {
+//   console.log(message)
+// }
+
+// or:
+
+// let doLog = (message) => console.log(message);
+
+// ***************************************************************************
+// interfaces
+
+// avoid to many properties...encapsulate in object
+// let drawPoint = (x, y, a, b, c, d, e) => {
+//   //..
+// }
+
+// like this:
+
+// let drawPoint = (point) => {
+//   //...
+// }
+// drawPoint({
+//   x: 1,
+//   y: 2
+// })
+
+
+// or interface like this:
+// defining shape of object
+// interface Point {
+//   x: number,
+//   y: number
+// }
+// let drawPoint = (point: Point) => {
+//   //..
+// }
+
+// ***************************************************************************
+// Classes - groups variables (properties) and fuctions (methods)
+// that are highly relate.
+
+// Cohesion = things should go together which we violated above
+
+// let getDistance = (pointA: Point, pointB: Point) =>
+
+class Point{
+  x:number;
+  y:number;
+
+  draw() {
+    console.log('x: ' + this.x + ', Y: ' + this.y);
+  }
+  getDistance(another:Point) {
+    //..,
+  }
+}
+
+// objects
+// when defining object of custom type you must allocate memory using 'new' operator like this:
+let point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
+
+
+// interface Point {
+//   x: number,
+//   y: number,
+//   draw:() => draw
+// }
+// let drawPoint = (point: Point) => {
+//   //..
+// }
+
+
+
+
+
+
+// ***************************************************************************
